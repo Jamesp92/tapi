84097e186030a13f40fa24aaca54a8ca6f716fbe
James Perry <PjPerry28@gmail.com>
2022-11-28 22:37:17 -0500

    create archive table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_031626) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_033333) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "archives", force: :cascade do |t|
+    t.string "style"
+    t.string "brand"
+    t.string "brewery"
+    t.date "date_received"
+    t.boolean "priority", default: false
+    t.decimal "abv", precision: 8, scale: 2
+    t.decimal "price", precision: 8, scale: 2
+    t.integer "serving_size"
+    t.date "date_tapped"
+    t.boolean "partial", default: false
+    t.integer "serving_price"
+    t.integer "keg_size"
+    t.date "date_kicked"
+    t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "inventory_kegs", force: :cascade do |t|
     t.string "style"
