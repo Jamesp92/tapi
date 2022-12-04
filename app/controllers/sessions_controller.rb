4e6cee99fd07b9e53bf674d19e4ee7fa91489dbb
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-12-04 15:35:57 -0800

    after login, redirect to root

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(user_name: params[:user_name])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to @user
+      redirect_to root_path
     else  
       redirect_to login_path
     end
@@ -16,5 +16,5 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to login_path
   end
-  
+
 end
