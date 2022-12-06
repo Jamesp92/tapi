12dda2ad9ba802ed498ac3d414d6168ba530362f
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-12-06 01:27:48 -0800

    add custom kick route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   post "/sessions", to: "sessions#create"
   delete "/sessions", to: "sessions#destroy"
   
+  post "/kick", to: "taps#kick"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
