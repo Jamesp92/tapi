723d6bd3f6286cd02d576549674e22b0e6da1fac
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-12-06 01:27:34 -0800

    add bootstrap assets

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,5 @@
 pin "application", preload: true
 pin "sortablejs", to: "https://ga.jspm.io/npm:sortablejs@1.15.0/modular/sortable.esm.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
