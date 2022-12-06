f76ce629858cf3f6e76462dd0ab7964254d3abc2
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-12-06 05:39:21 -0800

    finish nex keg for tap selection method

--- app/models/inventory_keg.rb
+++ app/models/inventory_keg.rb
@@ -3,23 +3,23 @@ class InventoryKeg < ApplicationRecord
   
   scope :most_recent, -> { order(created_at: :desc).limit(5)}
 
-  def self.find_next_keg(tap_id)
-    if InventoryKeg.where("taps_id = ?", "#{tap_id}").any?
-      if InventoryKeg.where("taps_id = ?", "#{tap_id}").where(priority: true).any?
-        InventoryKeg.where("taps_id = ?", "#{tap_id}").where(priority: true).order(:date_received).first
-      elsif InventoryKeg.where("taps_id = ?", "#{tap_id}").where(partial: true).any?
-        InventoryKeg.where("taps_id = ?", "#{tap_id}").where(partial: true).order(:date_received).first
+  def self.find_next_keg(tap)
+    if InventoryKeg.where("taps_id = ?", tap.id).any?
+      if InventoryKeg.where("taps_id = ?", tap.id).where(priority: true).any?
+        InventoryKeg.where("taps_id = ?", tap.id).where(priority: true).order(:date_received).first
+      elsif InventoryKeg.where("taps_id = ?", tap.id).where(partial: true).any?
+        InventoryKeg.where("taps_id = ?", tap.id).where(partial: true).order(:date_received).first
       else
-        InventoryKeg.where("taps_id = ?", "#{tap_id}").order(:date_received).first
+        InventoryKeg.where("taps_id = ?", tap.id).order(:date_received).first
       end
     
-    elsif InventoryKeg.where("style = ?", "#{tap_style}").any?
-      if InventoryKeg.where("style = ?", "#{tap_style}").where(priority: true).any?
-        InventoryKeg.where("style = ?", "#{tap_style}").where(priority: true).order(:date_received).first
-      elsif InventoryKeg.where("style = ?", "#{tap_style}").where(partial: true).any?
-        InventoryKeg.where("style = ?", "#{tap_style}").where(partial: true).order(:date_received).first
+    elsif InventoryKeg.where("style = ?", tap.tap_style).any?
+      if InventoryKeg.where("style = ?", tap.tap_style).where(priority: true).any?
+        InventoryKeg.where("style = ?", tap.tap_style).where(priority: true).order(:date_received).first
+      elsif InventoryKeg.where("style = ?", tap.tap_style).where(partial: true).any?
+        InventoryKeg.where("style = ?", tap.tap_style).where(partial: true).order(:date_received).first
       else
-        InventoryKeg.where("style = ?", "#{tap_style}").order(:date_received).first
+        InventoryKeg.where("style = ?", tap.tap_style).order(:date_received).first
       end
 
     else
@@ -31,7 +31,7 @@ class InventoryKeg < ApplicationRecord
         InventoryKeg.order(:date_received).first
       end
     end
-    
+
   end
   
 end
