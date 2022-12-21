ad87851e7f4fdf01f191c4a130ddc5ce3d5ea7d1
James Perry <PjPerry28@gmail.com>
2022-12-20 20:37:48 -0800

    prep for main branch merge

--- app/controllers/taps_controller.rb
+++ app/controllers/taps_controller.rb
@@ -1,13 +1,13 @@
 class TapsController < ApplicationController
 
   def index
-    @taps = Tap.all
+    @taps = Tap.all.order(:tap_number)
     render :all_taps
   end
 
   def show
     @tap = Tap.find(params[:id])
-    @tap_kegs = InventoryKeg.where("taps_id = ?", "#{params[:id]}").where.not(position: [0,1])
+    @tap_kegs = InventoryKeg.where("taps_id = ?", "#{params[:id]}")
     @tap_suggest = InventoryKeg.where("style = ?", "#{@tap.tap_style}").where(taps_id: nil).order(date_received: :desc)
     render :show
   end
